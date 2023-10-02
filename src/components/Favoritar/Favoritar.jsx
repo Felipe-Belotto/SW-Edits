886af65bf9fdc5160db762a806ec1ed723aa6254
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-10-02 12:07:15 -0300

    Revertendo commit anterior

--- src/components/Favoritar/Favoritar.jsx
+++ src/components/Favoritar/Favoritar.jsx
@@ -1,22 +1,48 @@
 import { Button } from '@mui/material';
+import styles from './Favoritar.module.css';
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import GradeIcon from '@mui/icons-material/Grade';
 import { useFavoritos } from '../../context/VideosFavoritos';
-import { useState } from 'react';
-import styles from './Favoritar.module.css'; // Certifique-se de ter o arquivo CSS correto
+import { useEffect, useState } from 'react';
 
 export default function Favoritar(props) {
-  const { adicionarFavorito, removerFavorito, limpaFavoritos, videosFavoritos } = useFavoritos();
+  const { adicionarFavorito, removerFavorito,limpaFavoritos, videosFavoritos } = useFavoritos();
   const [estadoFavorito, setEstadoFavorito] = useState(false);
+
+  const botaoFavoritar = {
+    display: "flex",
+    gap: "16px",
+    backgroundColor: "rgba(86, 86, 86, 0.26)",
+    color: "white",
+    textTransform: "none"
+  };
+
+  const botaoFavoritado = {
+    display: "flex",
+    gap: "16px",
+    backgroundColor: "rgba(122, 122, 122, 0.26)",
+    color: "orange",
+    textTransform: "none"
+  };
+
+
+const iconeFavoritar = <StarOutlineIcon />;
+const iconeFavoritado = <GradeIcon />;
+
 
   const aoClicar = () => {
     setEstadoFavorito(!estadoFavorito);
-    // Adicione lógica para adicionar/remover dos favoritos conforme necessário
+
+  /*   if (estadoFavorito === false) {
+      removerFavorito(props.id); // Substitua 'video.id' com o identificador real do vídeo
+    } else {
+      adicionarFavorito(props.video); // Substitua 'video' com o objeto do vídeo real
+    } */
   };
 
   return (
-    <Button onClick={aoClicar} className={estadoFavorito ? styles.botaoFavoritado : styles.botaoFavoritar}>
-      {estadoFavorito ? <GradeIcon /> : <StarOutlineIcon />}
+    <Button onClick={aoClicar} style={estadoFavorito ? botaoFavoritado : botaoFavoritar}>
+    {estadoFavorito ? iconeFavoritado : iconeFavoritar}
     </Button>
   );
 }
