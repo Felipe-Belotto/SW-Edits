e1dc91502d4b5cba7712d73034c84e33977e8adc
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-10-05 19:37:12 -0300

    adicionando clique fora do botao

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styles from './BurgerMenu.module.css';
 import Hamburger from 'hamburger-react';
 import { Link } from 'react-router-dom';
@@ -6,22 +6,48 @@ import BotaoLink from '../BotaoLink/BotaoLink';
 
 export default function BurgerMenu(props) {
   const [isOpen, setOpen] = useState(false);
+  const menuRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    const aoClicarFora = (event) => {
+      if (
+        isOpen &&
+        menuRef.current &&
+        !menuRef.current.contains(event.target) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target)
+      ) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener('click', aoClicarFora);
+    
+    return () => {
+      document.removeEventListener('click', aoClicarFora);
+    };
+  }, [isOpen]);
 
   return (
     <div className={styles.container}>
-      <button onClick={props.onClick} className={styles.botao}>
+      <button
+        ref={buttonRef}
+        onClick={() => setOpen(!isOpen)}
+        className={styles.botao}
+      >
         <Hamburger toggled={isOpen} toggle={setOpen} />
       </button>
       
       {isOpen && (
-        <nav className={styles.menu}>
+        <nav ref={menuRef} className={styles.menu}>
           <BotaoLink label={"Inicio"} link="/"/>
           <BotaoLink label={"Favoritos"} link="/favoritos"/>
           <div className={styles.adm}>
             <h1 className={styles.titulo}>Administração</h1>    
             <BotaoLink label="Novo vídeo" link="/novovideo" />
             <BotaoLink label="Nova categoria" link="/novacategoria" />
-            </div>
+          </div>
         </nav>
       )}
     </div>
