d6ba30faec11405f1f3e1ec99ac114d0cb520d9b
Felipe Belotto <felipeeduardo817@gmail.com>
2023-10-05 13:02:26 -0300

    adicionando linear gradiente no fundo da pagina inicial

--- src/Pages/PaginaInicial/PaginaInicial.jsx
+++ src/Pages/PaginaInicial/PaginaInicial.jsx
@@ -6,7 +6,6 @@ import { motion, useMotionValue } from 'framer-motion';
 export default function PaginaInicial() {
   const [videos, setVideos] = useState([]);
   const [categorias, setCategorias] = useState([]);
-  const scrollX = useMotionValue(0);
   const [imagemCapaIndex, setImagemCapaIndex] = useState(0);
 
   useEffect(() => {
@@ -18,16 +17,13 @@ export default function PaginaInicial() {
   const ajustarOpacidade = (corHex, fatorPreto, opacidade) => {
     const cleanedHex = corHex.replace('#', '');
     const [r, g, b] = cleanedHex.match(/.{1,2}/g).map((value) => parseInt(value, 16));
-  
-    // Adicionar mais preto à cor
+
     const novoR = Math.round(r * (1 - fatorPreto));
     const novoG = Math.round(g * (1 - fatorPreto));
     const novoB = Math.round(b * (1 - fatorPreto));
-  
+
     return `rgba(${novoR}, ${novoG}, ${novoB}, ${opacidade})`;
   };
-  
-  
 
   useEffect(() => {
     fetch('https://6516db6809e3260018ca679b.mockapi.io/Categorias')
@@ -36,14 +32,13 @@ export default function PaginaInicial() {
         setCategorias(
           dados.map((categoria) => ({
             ...categoria,
-            corDeFundo: ajustarOpacidade(categoria.cor,0.9 , 0.9),
+            corDeFundo: ajustarOpacidade(categoria.cor, 0.9, 0.9),
           }))
         );
       });
   }, []);
 
   useEffect(() => {
-    
     const intervaloID = setInterval(() => {
       setImagemCapaIndex((index) => (index + 1) % imagensCapa.length);
     }, 5000);
@@ -57,11 +52,21 @@ export default function PaginaInicial() {
     "https://media4.giphy.com/media/FHWLCr4nIJyOMar5eh/giphy.gif?cid=ecf05e47ca2f92c95i1xca6ut57ofruzrki00th9oqiyg6li&ep=v1_gifs_related&rid=giphy.gif&ct=g",
     "https://media1.giphy.com/media/5H9mE4GpY9UwTsIAXy/giphy.gif",
     "https://us.v-cdn.net/6025736/uploads/editor/1f/ujlf9r0xlxgn.gif",
-    "https://j.gifs.com/98W0lz@facebook.gif", 
+    "https://j.gifs.com/98W0lz@facebook.gif",
   ];
+
+  const scrollX = useMotionValue(0);
 
   const handleDrag = (_, info) => {
     scrollX.set(info.offset.x);
+  };
+
+  const linearGradienteCategorias = (index) => {
+    if (index < categorias.length - 1) {
+      return `linear-gradient(to bottom, ${categorias[index].corDeFundo} 90%, ${categorias[index + 1].corDeFundo})`;
+    } else {
+      return categorias[index].corDeFundo;
+    }
   };
 
   return (
@@ -70,11 +75,11 @@ export default function PaginaInicial() {
         <img src={imagensCapa[imagemCapaIndex]} className={styles.capa} />
       </div>
 
-      {categorias.map((categoria) => (
+      {categorias.map((categoria, index) => (
         <motion.section
           key={categoria.id}
           className={styles.categoriaContainer}
-          style={{ backgroundColor: categoria.corDeFundo }}
+          style={{ background: linearGradienteCategorias(index) }}
         >
           <div className={styles.categoria__info}>
             <h1 className={styles.categoria__nome}>{categoria.nome}</h1>
@@ -88,7 +93,6 @@ export default function PaginaInicial() {
               overflowX: 'auto',
               scrollSnapType: 'x mandatory',
               x: scrollX,
-              /* background: 'rgba(0, 0, 0, 0.293)', */
             }}
           >
             {videos
