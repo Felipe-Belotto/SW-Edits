a71227f04c35ffb2713f2d1f53c8f3f94613f3ac
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-10-06 12:36:39 -0300

    alterando tamanho do espaçamento nome da categoria

--- src/Pages/PaginaInicial/PaginaInicial.jsx
+++ src/Pages/PaginaInicial/PaginaInicial.jsx
@@ -42,7 +42,7 @@ export default function PaginaInicial() {
         setCategorias(
           dados.map((categoria) => ({
             ...categoria,
-            corDeFundo: ajustarOpacidade(categoria.cor, 0.9, 0.4),
+            corDeFundo: ajustarOpacidade(categoria.cor, 0.8, 0.4),
           }))
         );
       });
