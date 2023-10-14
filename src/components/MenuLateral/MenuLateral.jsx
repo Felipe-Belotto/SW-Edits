0b118701c2d9ac539318f5c4a2051cae2293a6f8
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-10-14 04:07:04 -0300

    adicionando contatos embaixo na navbar

--- src/components/MenuLateral/MenuLateral.jsx
+++ src/components/MenuLateral/MenuLateral.jsx
@@ -12,39 +12,71 @@ export default function MenuLateral(props) {
       style={{ display: props.display }}
     >
       <nav className={styles.nav}>
-        <section className={styles.nav__section}>
-          <MenuItem
-            label="Inicio"
-            to="/"
-            img={<i className="fa-solid fa-house"></i>}
-          />
-          <MenuItem
-            label="Favoritos"
-            to="/favoritos"
-            img={<i className="fa-solid fa-heart"></i>}
-          />
-        </section>
+        <div className={styles.div__main}>
+          <section className={styles.nav__section}>
+            <MenuItem
+              label="Inicio"
+              to="/"
+              img={<i className="fa-solid fa-house"></i>}
+            />
+            <MenuItem
+              label="Favoritos"
+              to="/favoritos"
+              img={<i className="fa-solid fa-heart"></i>}
+            />
+          </section>
 
-        <section className={styles.nav__section}>
-          <MenuItem
-            label="Add categoria"
-            to="/novacategoria"
-            img={<i className="fa-solid fa-list"></i>}
-          />
-          <MenuItem
-            label="Add video"
-            to="/novovideo"
-            img={<i className="fa-solid fa-video"></i>}
-          />
-        </section>
+          <section className={styles.nav__section}>
+            <MenuItem
+              label="Add categoria"
+              to="/novacategoria"
+              img={<i className="fa-solid fa-list"></i>}
+            />
+            <MenuItem
+              label="Add video"
+              to="/novovideo"
+              img={<i className="fa-solid fa-video"></i>}
+            />
+          </section>
 
-        <section className={styles.nav__section}>
-          <MenuItem
-            label="Painel de controle"
-            to="/controle"
-            img={<i className="fa-solid fa-computer"></i>}
-          />
-        </section>
+          <section className={styles.nav__section}>
+            <MenuItem
+              label="Painel de controle"
+              to="/controle"
+              img={<i className="fa-solid fa-computer"></i>}
+            />
+          </section>
+        </div>
+
+        <div className={styles.div__footer}>
+          <h1>Felipe Belotto</h1>
+          <div className={styles.div__contatos}>
+            <a
+              href="https://github.com/Felipe-Belotto"
+              className={styles.contato__icone}
+              target="_blank
+            "
+            >
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/felipe-belotto-a34738185/"
+              className={styles.contato__icone}
+              target="_blank
+            "
+            >
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a
+              href="https://github.com/Felipe-Belotto"
+              className={styles.contato__icone}
+              target="_blank
+            "
+            >
+              <i class="fa-solid fa-envelope"></i>
+            </a>
+          </div>
+        </div>
       </nav>
       <button
         onClick={() => {
