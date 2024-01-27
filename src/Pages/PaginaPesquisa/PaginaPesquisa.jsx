5783f4f5950a7feae1892cb093899a3d7a4f370d
Felipe-Belotto <felipeeduardo817@gmail.com>
2024-01-27 00:12:59 -0300

    Adicionando função de pesquisar na seção de pesquisa

--- src/Pages/PaginaPesquisa/PaginaPesquisa.jsx
+++ src/Pages/PaginaPesquisa/PaginaPesquisa.jsx
@@ -1,5 +1,70 @@
+
 import styles from './PaginaPesquisa.module.css';
+import Card from '../../components/Card/Card';
+import { useEffect, useState } from 'react';
+import { InputPesquisa } from '../../components/InputPesquisa/InputPesquisa';
 
 export default function PaginaPesquisa() {
-  return <h1 className={styles.titulo}>Em construção</h1>;
+
+  const [videosRelacionados, setVideosRelacionados] = useState([]);
+  const [textoPesquisado, setTextoPesquisado] = useState("");
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const carregarDados = async () => {
+    try {
+      const respostaVideos = await fetch(
+        `https://6516db6809e3260018ca679b.mockapi.io/Edits`,
+      );
+      const dadosVideos = await respostaVideos.json();
+      setVideos(dadosVideos);
+    } catch (error) {
+      console.error('Erro ao carregar dados:', error);
+      setLoading(false);
+    }}
+
+
+  useEffect(() => {
+    carregarDados();
+  }, []);
+
+  function localizaVideos() {
+    const videosFiltrados = videos.filter((video) =>
+      video.titulo.includes(textoPesquisado)
+    );
+    setVideosRelacionados((prevVideos) => [...videosFiltrados]);
+    console.log(videosFiltrados);
+  }
+  
+  function listarNomesPersonagens() {
+    const nomesPersonagens = videos.map((video) => video.titulo);
+    const nomesOrdenados = [...new Set(nomesPersonagens)].sort();
+    return nomesOrdenados;
+  }
+
+   useEffect(() => {
+     localizaVideos();
+   }, [textoPesquisado, videos]);
+  
+  return (
+  
+    <section className={styles.container}>
+      <div className={styles.container__inputPesquisa}>
+      <InputPesquisa
+  value={textoPesquisado}
+  onChange={(value) => setTextoPesquisado(value)}
+  opcoes={listarNomesPersonagens()}
+/>
+
+      </div>
+
+      <div className={styles.container__cards}>
+    {videosRelacionados.length > 0 && (
+    videosRelacionados.map((videoAtual) => <Card key={videoAtual.id} {...videoAtual} />)
+   )}
+      </div>
+
+    </section>
+  
+  );
 }
---
+++ src/components/InputPesquisa/InputPesquisa.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './InputPesquisa.module.css';
+import { Autocomplete, TextField } from '@mui/material';
+
+export const InputPesquisa = (props) => {
+  const { value, onChange, opcoes } = props;
+
+  const opcoesComVazio = [""].concat(opcoes);  
+
+  return (
+    <Autocomplete
+      value={value}
+      onChange={(event, newValue) => {
+        onChange(newValue);
+      }}
+      disablePortal
+      id="combo-box-demo"
+      options={opcoesComVazio} 
+      sx={{ width: 300, background: "#f4f4f4" }}
+      ListboxProps={{ style: { background: "rgb(49, 49, 49)" } }}
+      renderInput={(params) => (
+        <TextField label="Pesquisa" {...params} variant='filled'/>
+      )}
+    />
+  );
+};
